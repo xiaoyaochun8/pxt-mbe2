5b56a4c716470b357a25d1203447a8d4d46f3290
Barry.C <wjyzxhm888@163.com>
2025-04-20 01:27:15 +0800

    更新 devVarType.ts

--- devVarType.ts
+++ devVarType.ts
@@ -1,6 +1,15 @@
 // 在此处添加您的代码
 namespace devVarType{
     //% block
-    export function getPos2(myOledPos: Image) {
+    export function devImage(myOledPos: Image) {
+    }
+    //% block
+    export function devArray(ary: string[]) {
+    }
+    //% block
+    export function devString(s: string) {
+    }
+    //% block
+    export function devNumber(n: number) {
     }
 }
