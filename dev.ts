e99b4b23b4233756e702036ee2c9bf9d96b67289
Barry.C <wjyzxhm888@163.com>
2025-04-20 09:47:57 +0800

    Update dev.ts

--- dev.ts
+++ dev.ts
@@ -15,4 +15,19 @@ namespace mydev{
     export function oledCharacterPixels(i: string): Image {
         return <Image><any>i;
     }
+
+    /**
+     * Create a 5x8 pixel matrix for use as a custom character.
+     */
+    //% subcategory="test"
+    //% block
+    export function getImgPixels(im: Image): void {
+        for (let y = 0; y < 19; y++) {
+            for (let x = 0; x < 19; x++) {
+                if (im.pixel(x, y)) {
+                    serial.writeLine(x.toString()+'-'+y.toString())
+                }
+            }
+        }
+    }
 }
