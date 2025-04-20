f7f802417637ccdf8110a02114ab3411e2812c20
Barry.C <wjyzxhm888@163.com>
2025-04-20 10:35:15 +0800

    Update dev.ts

--- dev.ts
+++ dev.ts
@@ -2,23 +2,10 @@
 //% icon="\uf04a"
 namespace mydev{
     /**
-     * Create a 128x18 pixel matrix for use as a custom character.
-     */
-    //% subcategory="oled"
-    //% block="character12818"
-    //% imageLiteral=1
-    //% imageLiteralColumns=128
-    //% imageLiteralRows=18
-    //% imageLiteralScale=0.001
-    //% shim=images::createImage
-    export function oledCharacterPixels12818(i: string): Image {
-        return <Image><any>i;
-    }
-    /**
      * Create a 128x64 pixel matrix for use as a custom character.
      */
     //% subcategory="oled"
-    //% block="character12864"
+    //% block="character128x64"
     //% imageLiteral=1
     //% imageLiteralColumns=128
     //% imageLiteralRows=64
@@ -31,13 +18,13 @@ namespace mydev{
      * Create a 19x19 pixel matrix for use as a custom character.
      */
     //% subcategory="oled"
-    //% block="character19"
+    //% block="character19x19"
     //% imageLiteral=1
     //% imageLiteralColumns=19
     //% imageLiteralRows=19
     //% imageLiteralScale=0.001
     //% shim=images::createImage
-    export function oledCharacterPixels(i: string): Image {
+    export function oledCharacterPixels1919(i: string): Image {
         return <Image><any>i;
     }
 
@@ -45,13 +32,13 @@ namespace mydev{
      * Create a 16x16 pixel matrix for use as a custom character.
      */
     //% subcategory="oled"
-    //% block="character16"
+    //% block="character16x16"
     //% imageLiteral=1
     //% imageLiteralColumns=16
     //% imageLiteralRows=16
     //% imageLiteralScale=0.001
     //% shim=images::createImage
-    export function oledCharacterPixels2(i: string): Image {
+    export function oledCharacterPixels1616(i: string): Image {
         return <Image><any>i;
     }
 
@@ -61,8 +48,8 @@ namespace mydev{
     //% subcategory="test"
     //% block
     export function getImgPixels(im: Image): void {
-        for (let y = 0; y < 19; y++) {
-            for (let x = 0; x < 19; x++) {
+        for (let y = 0; y < 64; y++) {
+            for (let x = 0; x < 128; x++) {
                 if (im.pixel(x, y)) {
                     serial.writeLine(x.toString()+'-'+y.toString())
                 }
