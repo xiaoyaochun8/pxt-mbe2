7563f389d02235882f542560179905359102ceba
Barry.C <wjyzxhm888@163.com>
2025-04-20 10:08:57 +0800

    Update dev.ts

--- dev.ts
+++ dev.ts
@@ -2,6 +2,32 @@
 //% icon="\uf04a"
 namespace mydev{
     /**
+     * Create a 128x18 pixel matrix for use as a custom character.
+     */
+    //% subcategory="oled"
+    //% block="character12818"
+    //% imageLiteral=1
+    //% imageLiteralColumns=128
+    //% imageLiteralRows=18
+    //% imageLiteralScale=0.001
+    //% shim=images::createImage
+    export function oledCharacterPixels12818(i: string): Image {
+        return <Image><any>i;
+    }
+    /**
+     * Create a 128x64 pixel matrix for use as a custom character.
+     */
+    //% subcategory="oled"
+    //% block="character12864"
+    //% imageLiteral=1
+    //% imageLiteralColumns=128
+    //% imageLiteralRows=64
+    //% imageLiteralScale=0.001
+    //% shim=images::createImage
+    export function oledCharacterPixels12864(i: string): Image {
+        return <Image><any>i;
+    }
+    /**
      * Create a 19x19 pixel matrix for use as a custom character.
      */
     //% subcategory="oled"
