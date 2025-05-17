4ad0a49585d78325910b2e7a81d116e6af4fd829
Barry.C <wjyzxhm888@163.com>
2025-05-17 08:48:17 +0800

    更新 pxt.json, dev.ts

--- dev.ts
+++ dev.ts
@@ -7,8 +7,8 @@ namespace mydev{
     //% subcategory="oled"
     //% block="character128x64"
     //% imageLiteral=1
-    //% imageLiteralColumns=128
-    //% imageLiteralRows=64
+    //% imageLiteralColumns=12
+    //% imageLiteralRows=6
     //% imageLiteralScale=0.001
     //% shim=images::createImage
     export function oledCharacterPixels12864(i: string): Image {
@@ -18,11 +18,12 @@ namespace mydev{
     //% subcategory="test"
     //% block
     export function getImgPixels(im: Image): void {
-        for (let y = 0; y < 64; y++) {
-            for (let x = 0; x < 128; x++) {
+        OLED12864_I2C.init(60)
+        for (let y = 0; y < 6; y++) {
+            for (let x = 0; x < 12; x++) {
                 if (im.pixel(x, y)) {
                     serial.writeLine(x.toString()+'-'+y.toString())
-                    //todo call < OLED12864_I2C.pixel(x, y, color) >
+                    OLED12864_I2C.pixel(x, y, 1)
                 }
             }
         }
