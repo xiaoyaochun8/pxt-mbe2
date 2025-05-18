10575c29ca545d06b64ef64b712429699c36d40c
Barry.C <wjyzxhm888@163.com>
2025-05-18 23:00:12 +0800

    更新 dev.ts

--- dev.ts
+++ dev.ts
@@ -1,4 +1,12 @@
 // 在此处添加您的代码
+enum OledFace{
+    //% block="^_^"
+    Happy,
+    //% block="^.^"
+    Sad,
+}
+
+
 //% icon="\uf04a"
 //% groups=['oled-shape','oled-face','oled-paint']
 namespace mydev{
@@ -33,15 +41,10 @@ namespace mydev{
     //% subcategory="oled"
     //% group='oled-face'
     //% block
-    //% iSize.min=1 iSize.max=3 iSize.defl=1
-    export function oledDrawFace(iSize: Number): void {
+    //% gesture.fieldEditor="gridpicker"
+    //% gesture.fieldOptions.width=220
+    //% gesture.fieldOptions.columns=3
+    export function oledDrawFace(oledFace: OledFace): void {
         OLED12864_I2C.init(60);
-        if (iSize == 3) {
-            OLED12864_I2C.rect(0, 0, 60, 30, 1);
-        } else if (iSize == 2) {
-            OLED12864_I2C.rect(0, 0, 40, 20, 1);
-        } else {
-            OLED12864_I2C.rect(0, 0, 20, 10, 1);
-        }
     }
 }
