b53d7dee3b22996cc382d5f1538a70f7513c60d5
Barry.C <wjyzxhm888@163.com>
2026-03-31 08:48:17 +0800

    更新 pxt.json, devVarType.ts

--- devVarType.ts
+++ devVarType.ts
@@ -1,10 +1,10 @@
 // 在此处添加您的代码
-namespace devVarType{
-    //% block
-    export function devImage(myOledPos: Image) {
-    }
+namespace devVarType {
     //% block
     export function devEvent(handler: () => void) {
+    }
+    //% block
+    export function devImage(myOledPos: Image) {
     }
     //% block
     export function devArray(ary: string[]) {
