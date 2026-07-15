fece753c4bbf9d71c20e5c4e407cad18bd75d694
Barry.C <wjyzxhm888@163.com>
2026-07-15 08:29:54 +0800

    更新 pxt.json, dev.ts

--- dev.ts
+++ dev.ts
@@ -3,4 +3,13 @@
 //% icon="\uf04a"
 namespace mydev{
 
+    enum MyDropList1 {
+        //% block="d1"
+        d1 = 1,
+        //% block="d2"
+        d2 = 2
+    }
+    //% block
+    export function dropList1(e: MyDropList1): void {
+    }
 }
