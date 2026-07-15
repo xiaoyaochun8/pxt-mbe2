81264dc19b8a7bb4b34fab165bd47d41be11dc70
cblvip <wjyzxhm888@163.com>
2026-07-15 15:59:53 +0800

    1

--- dev.ts
+++ dev.ts
@@ -12,4 +12,18 @@ namespace mydev{
     //% block
     export function dropList1(e: MyDropList1): void {
     }
+    
+    //% blockId=numberPicker2
+    //% block="$value"
+    //% blockHidden=true
+    //% value.fieldEditor="numberdropdown"
+    //% value.fieldOptions.decompileLiterals=true
+    //% value.fieldOptions.data='[["a",1],["b",2]]'
+    export function __numberPicker2(value: number): number {
+        return value;
+    }
+    //% block
+    //% value.shadow="numberPicker2"
+    export function dropList2(value: number) {
+    }
 }
